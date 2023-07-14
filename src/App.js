0e874230b47acc3ab153e528f28cc421af308f0b
Serhii Neliubov <84928517+JakeJenneth@users.noreply.github.com>
2023-07-14 18:16:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Todo/Loader/Loader";
 
 function App() {
-  let [people, setPeople] = useState([
-    {id: 1, name: 'Elena', completed: false},
-    {id: 2, name: 'Max', completed: true},
-    {id: 3, name: 'Vitya', completed: false},
-  ])
+  let [people, setPeople] = useState([])
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setPeople(todos)
+          setLoading(false)
+        }, 2000)
+      })
+  }, [])
 
   function checkTodo(id) {
     setPeople(people.map(person => {
@@ -24,10 +34,10 @@ function App() {
     setPeople(people.filter(person => person.id !== id))
   }
 
-  function addTodo(name) {
+  function addTodo(title) {
     const newPeople = {
       id: Date.now(),
-      name,
+      title,
       completed: false
     }
 
@@ -43,9 +53,10 @@ function App() {
   return (
     <Context.Provider value={value}>
       <div className="wrapper">
-        <h1>Todo List</h1>
+        <h1>Список действий</h1>
         <AddTodo onCreate={addTodo}/>
-        {people.length ? <TodoList people={people}/> : <p>No Todos!</p>}
+        {loading ? <Loader /> : null}
+        {people.length ? <TodoList people={people}/> : loading ? null : <p>Нет элементов!</p>}
       </div>
     </Context.Provider>
   );
